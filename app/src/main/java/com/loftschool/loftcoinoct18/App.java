ddf21dd83b78d3abeea927a2d3fd7ea17ebb1bf6
kisin <wolf_joker@inbox.ru>
2018-10-09 22:06:14 +0300

    Add ApiInitializer class

--- app/src/main/java/com/loftschool/loftcoinoct18/App.java
+++ app/src/main/java/com/loftschool/loftcoinoct18/App.java
@@ -2,20 +2,28 @@ package com.loftschool.loftcoinoct18;
 
 import android.app.Application;
 
+import com.loftschool.loftcoinoct18.data.api.Api;
+import com.loftschool.loftcoinoct18.data.api.ApiInitializer;
 import com.loftschool.loftcoinoct18.data.prefs.Prefs;
 import com.loftschool.loftcoinoct18.data.prefs.PrefsImpl;
 
 public class App extends Application {
+    private Api api;
     private Prefs prefs;
 
     public Prefs getPrefs() {
         return prefs;
     }
 
+    public Api getApi() {
+        return api;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         prefs = new PrefsImpl(this);
+        api = new ApiInitializer().init();
     }
 
 
