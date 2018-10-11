5bcb96022141028c8923ac6c722164e0ab9ec997
kisin <wolf_joker@inbox.ru>
2018-10-12 00:07:58 +0300

    Add BottomNavigation and was extended MainActivity

--- app/src/main/java/com/loftschool/loftcoinoct18/screens/main/MainActivity.java
+++ app/src/main/java/com/loftschool/loftcoinoct18/screens/main/MainActivity.java
@@ -2,13 +2,26 @@ package com.loftschool.loftcoinoct18.screens.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.loftschool.loftcoinoct18.R;
+import com.loftschool.loftcoinoct18.screens.main.rate.RateFragment;
 import com.loftschool.loftcoinoct18.screens.start.StartActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView navigation;
+
     public static void start(Context context) {
         Intent starter = new Intent(context, MainActivity.class);
         starter.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -19,5 +32,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        navigation.setOnNavigationItemSelectedListener(navigationListener);
+        navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+            }
+        });
+
+        if(savedInstanceState == null){
+            navigation.setSelectedItemId(R.id.menu_item_rate);
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.menu_item_accounts:
+//                showWalletsFragment();
+                break;
+
+                case R.id.menu_item_rate:
+                showRateFragment();
+                break;
+
+                case R.id.menu_item_converter:
+//                showConverterFragment();
+                break;
+            }
+            return true;
+        }
+    };
+
+    private void showRateFragment() {
+        RateFragment fragment = new RateFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/loftschool/loftcoinoct18/screens/main/rate/RateFragment.java
@@ -0,0 +1,22 @@
+package com.loftschool.loftcoinoct18.screens.main.rate;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.loftschool.loftcoinoct18.R;
+
+
+public class RateFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rate, container, false);
+    }
+
+}
