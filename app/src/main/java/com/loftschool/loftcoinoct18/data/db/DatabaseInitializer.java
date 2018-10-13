3f550c2deaad7f3a9c79935284e5dfd49069c8ea
kisin <wolf_joker@inbox.ru>
2018-10-13 16:37:23 +0300

    Add Database,AppDatabase classes and realisation DatabaseRoomImpl plus DatabaseInitializer

---
+++ app/src/main/java/com/loftschool/loftcoinoct18/data/db/DatabaseInitializer.java
@@ -0,0 +1,18 @@
+package com.loftschool.loftcoinoct18.data.db;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+
+import com.loftschool.loftcoinoct18.data.db.room.AppDatabase;
+import com.loftschool.loftcoinoct18.data.db.room.DatabaseRoomImpl;
+
+public class DatabaseInitializer {
+
+    public Database init(Context context){
+        AppDatabase appDatabase = Room
+                .databaseBuilder(context, AppDatabase.class, "LoftCoin.db")
+                .build();
+
+        return new DatabaseRoomImpl(appDatabase);
+    }
+}
---
+++ app/src/main/java/com/loftschool/loftcoinoct18/data/db/room/DatabaseRoomImpl.java
@@ -0,0 +1,25 @@
+package com.loftschool.loftcoinoct18.data.db.room;
+
+import com.loftschool.loftcoinoct18.data.db.Database;
+import com.loftschool.loftcoinoct18.data.db.model.CoinEntity;
+
+import java.util.List;
+
+public class DatabaseRoomImpl implements Database {
+
+    private AppDatabase database;
+
+    public DatabaseRoomImpl(AppDatabase database) {
+        this.database = database;
+    }
+
+    @Override
+    public void saveCoins(List<CoinEntity> coins) {
+        database.coinDao().saveCoins(coins);
+    }
+
+    @Override
+    public List<CoinEntity> getCoins() {
+        return database.coinDao().getCoins();
+    }
+}
